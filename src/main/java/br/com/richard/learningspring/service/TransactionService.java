237ae67eed113219b81cd8bf439afb69e7cb72a8
richzago97 <rich.czago@gmail.com>
2024-01-13 13:12:48 -0300

    feat: added new methods

--- src/main/java/br/com/richard/learningspring/service/TransactionService.java
+++ src/main/java/br/com/richard/learningspring/service/TransactionService.java
@@ -6,6 +6,9 @@ import br.com.richard.learningspring.model.Transaction;
 import br.com.richard.learningspring.model.User;
 import br.com.richard.learningspring.repository.TransactionRepository;
 import br.com.richard.learningspring.repository.UserRepository;
+
+import java.util.Objects;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -23,15 +26,26 @@ public class TransactionService {
     public Transaction createTransaction(final CreateTransactionDto transactionData) {
 
         final User foundPayer = userRepository.findById(transactionData.getPayer_id()).orElseThrow(() -> new AppException("User not found", HttpStatus.NOT_FOUND));
+
+        if (Objects.equals(foundPayer.getType(), "SELLER")) {
+            throw new AppException("invalid user type", HttpStatus.FORBIDDEN);
+        }
         final User foundPayee = userRepository.findById(transactionData.getPayee_id()).orElseThrow(() -> new AppException("User not found", HttpStatus.NOT_FOUND));
 
+        final float transactionValue = transactionData.getValue();
+
         final float payerCurrentBalance = foundPayer.getBalance();
+
+        if (payerCurrentBalance < transactionValue) {
+            throw new AppException("Payer balance not sufficient", HttpStatus.FORBIDDEN);
+        }        
+
         final float payeeCurrentBalance = foundPayee.getBalance();
 
-        foundPayer.setBalance(payerCurrentBalance - transactionData.getValue());
-        foundPayee.setBalance(payeeCurrentBalance + transactionData.getValue());
+        foundPayer.setBalance(payerCurrentBalance - transactionValue);
+        foundPayee.setBalance(payeeCurrentBalance + transactionValue);
 
-        final Transaction newTransaction = new Transaction(foundPayer, foundPayee, transactionData.getValue());
+        final Transaction newTransaction = new Transaction(foundPayer, foundPayee, transactionValue);
 
         return transactionRepository.save(newTransaction);
     }
