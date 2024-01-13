cc0bc81b652d3019d07103ab1bbb26de8a6afe18
richzago97 <rich.czago@gmail.com>
2024-01-13 11:36:57 -0300

    feat: added validations to fields name, cpf, email, password and type

--- src/main/java/br/com/richard/learningspring/dto/UserDto.java
+++ src/main/java/br/com/richard/learningspring/dto/UserDto.java
@@ -1,17 +1,28 @@
 package br.com.richard.learningspring.dto;
 
-import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class UserDto {
 
+    @NotEmpty(message = "name cannot be null")
+    @Size(max=92, message = "name must be lower then 92 characters long")
     private String name;
 
+    @NotEmpty(message = "cpf cannot be null")
+    @Size(max=11, message = "cpf must be lower then 11 characters long")
     private String cpf;
 
+    @NotEmpty(message = "email cannot be null")
+    @Size(max=62, message = "email must be lower then 62 characters long")
     private String email;
 
+    @NotEmpty(message = "password cannot be null")
     private String password;
 
+    @NotEmpty(message = "type cannot be null")
+    @Pattern(regexp = "COMMON|SELLER", message = "type must be COMMON or SELLER")
     private String type;
 
     public String getName() {
